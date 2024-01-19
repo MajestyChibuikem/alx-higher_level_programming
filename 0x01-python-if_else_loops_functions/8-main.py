46ae7d4d5d840581aa93cda59d3c53e30ba09ac3
“Majesty” <majestychibuikem@gmail.com>
2024-01-19 10:52:47 +0100

    holberton

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 uppercase = __import__('8-uppercase').uppercase
-uppercase("BEST")
-uppercase("BEST SCHOOL 98 BATTERY STREET")
+uppercase("holberton")
+uppercase("Holberton Scholl 98 Battery street")
